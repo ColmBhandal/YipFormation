386f57a4c19628d5af0818ef6934ed62c7d8e03c
Colm Bhandal <bhandalc@gmail.com>
2022-06-05 18:59:57 +0100

    Fixed Hello-World function
    Added package.json
    SAM build needs to run before SAM deploy for this to work
    
    Refactored function code
    Code now looks like online sample app
    Responses need a status code or they come back as 5xx errors
    Also, we need to await the async call to get account settings

--- functions/hello/index.js
+++ functions/hello/index.js
@@ -1,21 +1,49 @@
-import {AWSXRay} from 'aws-xray-sdk-core'
+const AWSXRay = require('aws-xray-sdk-core')
 const AWS = AWSXRay.captureAWS(require('aws-sdk'))
 
 // Create client outside of handler to reuse
 const lambda = new AWS.Lambda()
 
 // Handler
-export async function handler(event, context){
-  event.Records.forEach(record => {
-    console.log(record.body)
-  })
+exports.handler = async function(event, context) {
   console.log('## ENVIRONMENT VARIABLES: ' + serialize(process.env))
   console.log('## CONTEXT: ' + serialize(context))
   console.log('## EVENT: ' + serialize(event))
-  
-  return getAccountSettings()
+  try {
+    let accountSettings = await getAccountSettings()
+    return formatResponse(serialize(accountSettings.AccountUsage))
+  } catch(error) {
+    return formatError(error)
+  }
 }
 
+var formatResponse = function(body){
+  var response = {
+    "statusCode": 200,
+    "headers": {
+      "Content-Type": "application/json"
+    },
+    "isBase64Encoded": false,
+    "multiValueHeaders": { 
+      "X-Custom-Header": ["My value", "My other value"],
+    },
+    "body": body
+  }
+  return response
+}
+
+var formatError = function(error){
+  var response = {
+    "statusCode": error.statusCode,
+    "headers": {
+      "Content-Type": "text/plain",
+      "x-amzn-ErrorType": error.code
+    },
+    "isBase64Encoded": false,
+    "body": error.code + ": " + error.message
+  }
+  return response
+}
 // Use SDK client
 var getAccountSettings = function(){
   return lambda.getAccountSettings().promise()
