1da4c7626c1d7834641c1cc12b7abeefd672e046
Colm Bhandal <bhandalc@gmail.com>
2022-06-06 15:27:34 +0100

    Removed XRay tracing
    Can't test API locally with it
    For now, local API testing seems more important than XRay
    Also, it looks like XRay may be enabled by default anyway

--- functions/hello/index.js
+++ functions/hello/index.js
@@ -1,5 +1,4 @@
-const AWSXRay = require('aws-xray-sdk-core')
-const AWS = AWSXRay.captureAWS(require('aws-sdk'))
+const AWS = require('aws-sdk')
 
 // Create client outside of handler to reuse
 const lambda = new AWS.Lambda()
