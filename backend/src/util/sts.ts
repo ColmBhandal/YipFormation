6405e5a62e7546dbfa9ba16a6a985d53c9c72ea3
Colm Bhandal <bhandalc@gmail.com>
2022-07-20 11:54:11 +0100

    Formatting semicolons

--- backend/src/util/sts.ts
+++ backend/src/util/sts.ts
@@ -1,10 +1,10 @@
 import {STS} from "aws-sdk"
-import { STSClient } from  "@aws-sdk/client-sts";
+import { STSClient } from  "@aws-sdk/client-sts"
 
 export const sts = new STS()
 
 
 //Non-MVP: Should we be setting the region during build rather than hard-coding?
 const REGION = "us-east-1"
-const stsClient = new STSClient({ region: REGION });
-export { stsClient };
+const stsClient = new STSClient({ region: REGION })
+export { stsClient }
